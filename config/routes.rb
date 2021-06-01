235dd0ae4064d600e5f658a5018dbaa53e402e50
villysiu <villysiu@gmail.com>
2021-05-31 22:27:31 -0700

    devise setup

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+authenticated :user do
+    root 'home#index', as: 'authenticated_root'
+  end
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
+end
