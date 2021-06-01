7d7024747a28fda8acc4ff1014d893893834d385
villysiu <villysiu@gmail.com>
2021-06-01 11:46:05 -0700

    rails g resource Animal and view pages (index, show)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :animals
  # devise_for :users
  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 authenticated :user do
